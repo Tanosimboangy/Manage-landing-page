b069dccef09850528a28c442900c48993056e821
Tanosimboangy <jacquit.aja@onja.org>
2021-02-08 07:46:57 +0300

    converting react structure into simple html

--- dist/iwt2-manage-landing-page.e31bb0bc.js
+++ dist/iwt2-manage-landing-page.e31bb0bc.js
@@ -30036,7 +30036,9 @@ function Footer() {
     className: "footer"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "container"
-  }, /*#__PURE__*/_react.default.createElement("form", null, /*#__PURE__*/_react.default.createElement("input", {
+  }, /*#__PURE__*/_react.default.createElement("form", {
+    className: "footer_form"
+  }, /*#__PURE__*/_react.default.createElement("input", {
     type: "email",
     placeholder: "Updates in your inbox..."
   }), /*#__PURE__*/_react.default.createElement("button", {
@@ -30168,7 +30170,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59606" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62877" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
